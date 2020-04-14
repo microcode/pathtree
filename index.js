1ef3b68bb2e96c6330d1a7a3046a3037138a8069
Jesper Svennevid <jsvennevid@users.noreply.github.com>
2020-04-14 16:49:13 +0200

    Typescript fixes

--- index.js
+++
@@ -1,6 +0,0 @@
-const { PathNode } = require('./lib/node');
-const { PathTree } = require('./lib/tree');
-
-exports.PathNode = PathNode;
-exports.PathTree = PathTree;
-
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import {
+    IPathNode,
+    PathNode,
+} from "./PathNode";
+
+import {
+    PathTree
+} from "./PathTree";
+
+export {
+    IPathNode,
+    PathNode,
+    PathTree
+}
