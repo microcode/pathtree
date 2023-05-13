bcc2d1dbb9aaa2bde6547769ff4526332ae9d282
Jesper Svennevid <jsvennevid@users.noreply.github.com>
2023-05-14 00:48:01 +0200

    Add missing export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export { IPathNode } from "./IPathNode";
 export { PathNode } from "./PathNode";
-export { PathTree } from "./PathTree";
+
+export {
+    PathTree,
+    PathCapture
+} from "./PathTree";
